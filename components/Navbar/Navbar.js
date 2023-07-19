5566e5218a61830e35c8a653f9379bbe13e895ee
talhasaeed22 <talhasaeed100@gmail.com>
2023-07-19 23:23:41 +0500

    Process Added

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -35,9 +35,9 @@ const Navbar = () => {
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <i style={{ fontSize: '28px' }} className="fa fa-times block bg-[#6F3081] p-6 rounded-full text-gray-300 text-2xl " aria-hidden="true"></i>
+                    <i style={{ fontSize: '18px' }} className="fa fa-times block bg-[#6F3081] p-3 rounded-full text-gray-300 text-2xl " aria-hidden="true"></i>
                   ) : (
-                    <i style={{ fontSize: '28px' }} className="fa fa-bars block text-3xl bg-[#6F3081] p-6 rounded-full text-gray-200  " aria-hidden="true"></i>
+                    <i style={{ fontSize: '18px' }} className="fa fa-bars block bg-[#6F3081] p-3 rounded-full text-gray-200  " aria-hidden="true"></i>
                   )}
                 </Disclosure.Button>
               </div>
